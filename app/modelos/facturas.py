824984132c5020eaab2c1b6fa158d58f4e3faf1d
santiagosierra77 <santiagosierram17@gmail.com>
2026-07-10 23:03:21 -0500

    Tablas y relaciones

--- app/modelos/facturas.py
+++ app/modelos/facturas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, computed_field
-
+from sqlmodel import SQLModel, Field, Relationship
 from .transacciones import Transaccion
 from .clientes import Cliente
 from datetime import datetime
@@ -10,33 +10,37 @@ from datetime import datetime
 
 
 # Crear el modelo transacciones(id, fecha, vr_total, cliente)
-class FacturaBase(BaseModel):
-    fecha: str = datetime.now()
-    cliente: Cliente #esta es la relacion con el cliente(objeto)
-    transacciones: list[Transaccion] = []
+class FacturaBase(SQLModel):
+    fecha: str = Field(default=datetime.now())
+    #cliente: Cliente #esta es la relacion con el cliente(objeto)
+    #transacciones: list[Transaccion] = []
     
     @computed_field
     @property
     def vr_total(self) -> float:
-        #calcular(cantidad * vr_unitario)
-        #consultar el id actual de la factura
-        factura_id_actual = getattr(self, 'id', None)
-        total_factura = 0.0
-        if not factura_id_actual or not self.transacciones:
-            return total_factura
-        #recorrer la lista de transacciones, segun el factura_id 
-        for transaccion in self.transacciones:
-            if transaccion.factura_id == factura_id_actual:
-                total_factura += transaccion.vr_unitario * transaccion.cantidad
+        # #calcular(cantidad * vr_unitario)
+        # #consultar el id actual de la factura
+        # factura_id_actual = getattr(self, 'id', None)
+        # total_factura = 0.0
+        # if not factura_id_actual or not self.transacciones:
+        #     return total_factura
+        # #recorrer la lista de transacciones, segun el factura_id 
+        # for transaccion in self.transacciones:
+        #     if transaccion.factura_id == factura_id_actual:
+        #         total_factura += transaccion.vr_unitario * transaccion.cantidad
         
-        return total_factura
+        return 0.0
+
 
 class FacturaCrear(FacturaBase):
     pass
+
 
 class FacturaEditar(FacturaBase):
     pass
 
 
-class Factura(FacturaBase):
-    id: int | None = None
+class Factura(FacturaBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    cliente_id: int = Field(default=None, foreign_key="cliente.id")
+    
--- app/modelos/transacciones.py
+++ app/modelos/transacciones.py
@@ -1,20 +1,21 @@
 from pydantic import BaseModel
-
+from sqlmodel import SQLModel, Field, Relationship
 
 # Crear el modelo Transaccional(id, cantidad, vr_unitario, id_factura)
-class TransaccionBase(BaseModel):
-    cantidad: int
-    vr_unitario: float
-    
+class TransaccionBase(SQLModel):
+    cantidad: int = Field(default=0)
+    vr_unitario: float = Field(default=0.0)
 
 
 class TransaccionCrear(TransaccionBase):
     pass 
 
+
 class TransaccionEditar(TransaccionBase):
     pass
 
-class Transaccion(TransaccionBase):
-    id: int | None = None
-    factura_id: int | None = None 
+
+class Transaccion(TransaccionBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    factura_id: int | None = Field(default=None, foreign_key="factura.id")
     #aqui va la relación con el modelo factura(solo un campo)
