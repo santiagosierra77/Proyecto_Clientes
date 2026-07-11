8b0c14c3ab0937dc85e342d66bec4c752006ecc2
santiagosierra77 <santiagosierram17@gmail.com>
2026-07-10 22:03:09 -0500

    Instalacion sqlmodel parte 2 creacion de clientes y listar todos

--- app/enrutadores/clientes.py
+++ app/enrutadores/clientes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from ..modelos.clientes import Cliente, ClienteCrear, ClienteEditar
 from ..listas import lista_clientes
 from ..conexion_bd import Sesion_dependencia
+from sqlmodel import select
 
 rutas_clientes = APIRouter()
 # lista_clientes: list[Cliente] = []
@@ -9,8 +10,9 @@ rutas_clientes = APIRouter()
 
 # Endpoint, para obtener o listar todos los clientes
 @rutas_clientes.get("/clientes", response_model=list[Cliente])
-async def listar_clientes():
-    return lista_clientes
+async def listar_clientes(sesion: Sesion_dependencia):
+    lista_cli = sesion.exec(select (Cliente)).all() 
+    return lista_cli
 
 
 # Endpoint, para obtener o listar un solo cliente de la lista
--- app/main.py
+++ app/main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI, HTTPException, status
-from .modelos.clientes import Cliente, ClienteCrear, ClienteEditar
-from .modelos.facturas import Factura, FacturaCrear, FacturaEditar
-from .modelos.transacciones import Transaccion, TransaccionCrear, TransaccionEditar
 from .enrutadores.clientes import rutas_clientes
 from .enrutadores.facturas import rutas_facturas
 from .enrutadores.transacciones import rutas_transacciones
+from .conexion_bd import crear_tablas
 
-app = FastAPI()
+app = FastAPI(lifespan=crear_tablas)
 
-
-lista_clientes: list[Cliente] = []
-lista_facturas: list[Factura] = []
-lista_transacciones: list[Transaccion] = []
 
 # incluir ruta de clientes
 app.include_router(rutas_clientes, tags=["Clientes"])
